f681f75d60db7723177c5e87bac98c6afda0a3e6
Uticodes <etim.utibe@gmail.com>
2023-12-14 14:11:17 +0100

    Lint format

--- app/src/main/java/com/example/universitieslist/view/UniversitiesScreen.kt
+++ app/src/main/java/com/example/universitieslist/view/UniversitiesScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
