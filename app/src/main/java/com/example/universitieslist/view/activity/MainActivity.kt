a99b9fe7d9a3595cb966313b178101af895150f3
Uticodes <etim.utibe@gmail.com>
2024-01-22 16:41:56 +0100

    Implement ApiService

--- app/src/main/java/com/example/universitieslist/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/universitieslist/view/activity/MainActivity.kt
@@ -15,10 +15,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             UniversitiesListTheme {
                 Surface(
