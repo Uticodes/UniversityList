e78426cfa4041e07135d009e1f968449fbc3d386
Uticodes <etim.utibe@gmail.com>
2024-01-22 17:55:31 +0100

    Implement Repository

--- app/src/main/java/com/example/universitieslist/util/Constants.kt
+++ app/src/main/java/com/example/universitieslist/util/Constants.kt
@@ -2,4 +2,9 @@ package com.example.universitieslist.util
 
 object Constants {
     const val BASE_URL = "http://universities.hipolabs.com/"
+    const val COUNTRY_NAME = "Nigeria"
+
+    // Network Messages
+    const val UNKNOWN_ERROR = "Opps! Unknown error, kindly try again"
+    const val UNABLE_TO_FETCHING = "Unable to fetch universities for"
 }
---
+++ app/src/test/java/com/example/universitieslist/util/MockUniversityResponse.kt
@@ -0,0 +1,22 @@
+package com.example.universitieslist.util
+
+import com.example.universitieslist.data.model.UniversityResponse
+
+val mockUniversityResponses = listOf(
+    UniversityResponse(
+        webPages = listOf("http://www.testuniversity1.com"),
+        alphaTwoCode = "TU",
+        domains = listOf("test_university1.com"),
+        country = "Ghana",
+        name = "Fake University One",
+        stateProvince = "Fake Province 1"
+    ),
+    UniversityResponse(
+        webPages = listOf("http://www.testuniversity2.com"),
+        alphaTwoCode = "NU",
+        domains = listOf("test_university2.com"),
+        country = "Nigeria",
+        name = "Fake University Two",
+        stateProvince = "Fake Province 2"
+    )
+)
